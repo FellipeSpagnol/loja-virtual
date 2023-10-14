583773563cd07e154990047f46f37a01b2487a55
Fellipe Spagnol <fellipespagnol@gmail.com>
2023-10-14 18:59:15 -0300

    add __eq__

--- pessoa.py
+++ pessoa.py
@@ -4,3 +4,6 @@ class Pessoa:
         self.nome = nome
         self.email = email
         self.cpf = cpf
+
+    def __eq__(self, outro: "Pessoa") -> bool:
+        return (self.nome == outro.nome) or (self.email == outro.email) or (self.cpf == outro.cpf)
