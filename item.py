f8054e4f0b01303a03767624f6ca042feeea62b5
Fellipe Spagnol <fellipespagnol@gmail.com>
2023-10-13 21:28:56 -0300

    add Item

---
+++ item.py
@@ -0,0 +1,10 @@
+from produto import Produto
+
+
+class Item:
+    def __init__(self, produto: Produto, quantidade: int) -> None:
+        self.produto = produto
+        self.quantidade = quantidade
+
+    def custo(self) -> float:
+        return self.produto.valor() * self.quantidade
