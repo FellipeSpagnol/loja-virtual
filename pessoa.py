60254c1b17f83ea8c1969ae82cc8046b114691d2
Fellipe Spagnol <fellipespagnol@gmail.com>
2023-10-13 21:02:56 -0300

    pep8 format

--- pessoa.py
+++ pessoa.py
@@ -1,5 +1,5 @@
 class Pessoa:
-    
+
     def __init__(self, nome: str, email: str, cpf: str) -> None:
         self.nome = nome
         self.email = email
--- produto.py
+++ produto.py
@@ -1,35 +1,35 @@
 class Produto:
-    
+
     def __init__(self, nome: str, valor: float, categoria: str) -> None:
         self.nome = nome
         self._valor = valor
         self._estoque = 0
         self._desconto = 0
         self.categoria = categoria
-        
+
     def valor(self) -> float:
         return self._valor*(1 - self._desconto)
-    
+
     def registrar_aquisicao(self, quantidade: int) -> None:
         self._estoque += quantidade
-    
+
     def registrar_venda(self, quantidade: int) -> bool:
         if (self._estoque - quantidade) >= 0:
             self._estoque -= quantidade
             return True
         else:
             return False
-    
+
     def estoque(self) -> int:
         return self._estoque
-    
+
     def atualizar_desconto(self, desconto_novo: float) -> bool:
         if (0 <= desconto_novo <= 1):
             self._desconto = desconto_novo
             return True
         else:
             return False
-        
+
     def atualizar_valor(self, valor_novo: float) -> bool:
         if (valor_novo > 0):
             self._valor = valor_novo
